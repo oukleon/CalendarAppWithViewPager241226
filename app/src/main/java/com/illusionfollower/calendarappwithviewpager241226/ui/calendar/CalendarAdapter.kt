7c59544390c4563f83215f976da763a6c6f8e530
oukle <kiseok.yang78@myhunter.cuny.edu>
2024-12-27 22:38:06 +0900

    second phase

--- app/src/main/java/com/illusionfollower/calendarappwithviewpager241226/ui/calendar/CalendarAdapter.kt
+++ app/src/main/java/com/illusionfollower/calendarappwithviewpager241226/ui/calendar/CalendarAdapter.kt
@@ -1,15 +1,26 @@
 package com.illusionfollower.calendarappwithviewpager241226.ui.calendar
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.illusionfollower.calendarappwithviewpager241226.R
 import com.illusionfollower.calendarappwithviewpager241226.databinding.ItemCalendarDayBinding
 import java.time.LocalDate
+import java.time.ZoneId
 
 class CalendarAdapter(
     private var itemHeight:Int,
     private val dayList: List<LocalDate>
 ): RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>(){
+    // adapter 레벨에서 한 번만 계산
+    private val today = LocalDate.now(ZoneId.systemDefault())
+    // adapter 레벨의 변수로 선언
+    private var todayDrawable: Drawable? = null
+    private var todayDrawableAlpha: Drawable? = null
+    private val transparentColor = Color.TRANSPARENT
 
     inner class CalendarViewHolder(private val binding: ItemCalendarDayBinding
     ):RecyclerView.ViewHolder(binding.root) {
@@ -29,6 +40,18 @@ class CalendarAdapter(
                 binding.tvDayText.text = date.dayOfMonth.toString()
                 currentDate = date
             }
+
+            val isToday = date == today
+            if (binding.clDay.tag != isToday){
+                binding.clDay.tag = isToday
+                if (isToday){
+                    binding.clDay.foreground = if (date.monthValue == today.dayOfMonth){
+                        todayDrawable
+                    }else{
+                        todayDrawableAlpha
+                    }
+                }
+            }
         }
     }
 
@@ -39,6 +62,13 @@ class CalendarAdapter(
                 false
         )
         binding.root.layoutParams.height = itemHeight
+        // ViewHolder 생성 시 Drawable 초기화
+        if (todayDrawable == null) {
+            todayDrawable = ContextCompat.getDrawable(parent.context, R.drawable.today_border)
+            todayDrawableAlpha = ContextCompat.getDrawable(parent.context, R.drawable.today_border)?.mutate()?.apply {
+                alpha = (0.5 * 255).toInt()
+            }
+        }
         return CalendarViewHolder(binding)
     }
 
